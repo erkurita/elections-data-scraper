3709b4bd09e402555ff1758de4d8ed23a2422851
Alejandro Carrilo <j.alec.n@gmail.com>
2013-05-02 23:49:13 +0200

    Fixed small indent

--- lib/scraper.py
+++ lib/scraper.py
@@ -31,30 +31,30 @@ from os import path
 #
 
 try:
-  from lxml import etree
-  print("running with lxml.etree")
+    from lxml import etree
+    print("running with lxml.etree")
 except ImportError:
-  try:
-    # Python 2.5
-    import xml.etree.cElementTree as etree
-    print("running with cElementTree on Python 2.5+")
-  except ImportError:
     try:
-      # Python 2.5
-      import xml.etree.ElementTree as etree
-      print("running with ElementTree on Python 2.5+")
+    # Python 2.5
+        import xml.etree.cElementTree as etree
+        print("running with cElementTree on Python 2.5+")
     except ImportError:
-      try:
-        # normal cElementTree install
-        import cElementTree as etree
-        print("running with cElementTree")
-      except ImportError:
         try:
-          # normal ElementTree install
-          import elementtree.ElementTree as etree
-          print("running with ElementTree")
+            # Python 2.5
+            import xml.etree.ElementTree as etree
+            print("running with ElementTree on Python 2.5+")
         except ImportError:
-          raise ImportError("Failed to import ElementTree from any known place, needed for the script to operate")
+            try:
+                # normal cElementTree install
+                import cElementTree as etree
+                print("running with cElementTree")
+            except ImportError:
+                try:
+                    # normal ElementTree install
+                    import elementtree.ElementTree as etree
+                    print("running with ElementTree")
+                except ImportError:
+                    raise ImportError("Failed to import ElementTree from any known place, needed for the script to operate")
 #
 # End of snippet
 #
